48ecfc574facaf05636bdde274a42001e50f1211
Corina <49889976+CoCoCorina@users.noreply.github.com>
2021-02-03 15:37:42 +0800

    mybatis

---
+++ MybatisPlus_Study/src/main/java/com/example/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.example.mybatisplus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.mybatisplus.po.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
